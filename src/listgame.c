193eb42f3812ac6b4ab7e7f5024956b8e1cabf2a
Wentonn <64532578+Wentonn@users.noreply.github.com>
2022-11-02 00:09:03 +0700

    ListGame

---
+++ src/listgame.c
@@ -0,0 +1,14 @@
+#include "listgame.h"
+
+void listgame(Array array_game)
+/*
+I.S : Array array_game terdefinisi
+F.S : Tercetak list game yang tersedi
+*/
+{
+    int i;
+    for (i=0;i<length(array_game);i++)
+    {
+        printf("%d %s\n",i+1,array_game.TI[i]);
+    }
+}
