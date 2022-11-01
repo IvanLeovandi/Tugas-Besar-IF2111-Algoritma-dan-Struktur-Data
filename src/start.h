14a571591ba0fefa3c290deac06b8ccf6ac070ef
Wentonn <64532578+Wentonn@users.noreply.github.com>
2022-11-01 20:25:36 +0700

    Create start.h

---
+++ src/start.h
@@ -0,0 +1,16 @@
+//File start.h
+#include <stdio.h>
+#include "adt/array.h"
+#include "adt/mesinkarakter.h"
+#include "adt/mesinkata.h"
+
+#ifndef START_H
+#define START_H
+
+void (ArrayGame *a);
+/*
+I.S. ArrayGame a terdefinisi, ArrayGame a kosong;
+F.S. ArrayGame a terisi dengan game yang berada pada file config.txt
+*/
+
+#endif
