2304dc84e4eb71297a89665eef25a2edae4e6bed
Wentonn <64532578+Wentonn@users.noreply.github.com>
2022-11-03 17:38:40 +0700

    Quit Game

---
+++ src/quitgame.c
@@ -0,0 +1,10 @@
+#include "quitgame.h"
+
+void listgame(Queue *queue_game)
+/*
+I.S : Queue queue_game terdefinisi
+F.S : Queue queue_game menjadi kosong dan keluar dari program
+*/
+{
+    CreateQueue(queue_game);
+}
