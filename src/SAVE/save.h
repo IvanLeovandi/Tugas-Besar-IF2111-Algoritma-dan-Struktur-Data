32cd4233292ce879e60a27cbc8151828d3be153d
frendysanusi05 <frendyvivo5@gmail.com>
2022-11-10 23:13:25 +0700

    save fixed

--- src/SAVE/save.h
+++ src/SAVE/save.h
@@ -6,12 +6,18 @@
 #ifndef SAVE_H
 #define SAVE_H
 
-#define InitialSize 1000
+#define InitialSize 10
 
 void save(char* filename, Array arrGame);
 /*
 I.S.: file *.txt kosong atau sudah terisi
 F.S.: State game tersave ke file *.txt
+*/
+
+void makeMallocEmpty(char* temp, int len);
+/*
+I.S.: temp hasil malloc berisi karakter-karakter sampah
+F.S.: temp menjadi kosong
 */
 
 #endif
