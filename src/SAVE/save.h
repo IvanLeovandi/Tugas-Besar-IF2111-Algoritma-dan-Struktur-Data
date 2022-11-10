07a35ae153ebcb7dccf57e8dc119272e08c67251
IvanLeovandi <16521021@std.stei.itb.ac.id>
2022-11-10 22:52:31 +0700

    update main dan save + nambahin komentar penjelasan di adt

--- src/SAVE/save.h
+++ src/SAVE/save.h
@@ -14,10 +14,4 @@ I.S.: file *.txt kosong atau sudah terisi
 F.S.: State game tersave ke file *.txt
 */
 
-// void makeMallocEmpty(char* temp, int len);
-// /*
-// I.S.: temp hasil malloc berisi karakter-karakter sampah
-// F.S.: temp menjadi kosong
-// */
-
 #endif
