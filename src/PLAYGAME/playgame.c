cbcd0a52a167b0a0b668951b28422a570c1f0fb1
IvanLeovandi <16521021@std.stei.itb.ac.id>
2022-11-11 00:28:20 +0700

    update play game

--- src/PLAYGAME/playgame.c
+++ src/PLAYGAME/playgame.c
@@ -9,22 +9,22 @@ F.S.: melakukan dequeue dan menyimpan hasil elemen yang baru dihapus di dalam na
 */
 {
     ElType val;
-    printf("Berikut adalah daftar Game-mu\n");
-    displayQueue(*game);
     if(isEmpty(*game))
     {
         printf("Silahkan tambahkan game ke antrian terlebih dahulu.\n");
     }
     else 
     {
+        printf("Berikut adalah daftar Game-mu\n");
+        displayQueue(*game);
         dequeue(game, &val);
         if(compareSTR(val, "RNG"))
         {
-            printf("Loading... %s\n", val);
+            printf("Loading %s ...\n", val);
             rng();
         } else if(compareSTR(val, "Diner DASH"))
         {
-            printf("Loading... %s\n", val);
+            printf("Loading %s ...\n", val);
             DinerDash();
         } 
         else if(compareSTR(val, "DINOSAUR IN EARTH") || compareSTR(val, "RISEWOMAN") || compareSTR(val, "EIFFEL TOWER"))
@@ -33,7 +33,7 @@ F.S.: melakukan dequeue dan menyimpan hasil elemen yang baru dihapus di dalam na
             printf("Silahkan pilih game lain.\n");
         } else if(compareSTR(val, "RPS"))
         {
-            printf("Loading... %s\n", val);
+            printf("Loading %s ...\n", val);
             Start_RPS();
         }
         else
