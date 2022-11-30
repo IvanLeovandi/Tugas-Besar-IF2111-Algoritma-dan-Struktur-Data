d384014873e3fba036e3161a3d30d769b602b54b
nadiraaraa <nadiraaraa@gmail.com>
2022-11-30 12:26:36 +0700

    help dan snake

--- src/HELP/help.c
+++ src/HELP/help.c
@@ -15,18 +15,32 @@ void help(boolean loaded){
         printf("---------------------------------------------------------------\n");
     } else
     {
-        printf("SAVE <filename>: Menyimpan keadaan game ke dalam file\n");
-        printf("CREATE GAME    : Menambahkan game baru pada daftar game\n");
-        printf("LIST GAME      : Menampilkan daftar game yang ada\n");
-        printf("DELETE GAME    : Menghapus sebuah game dari daftar game\n");
-        printf("                 Game yang dapat dihapus hanyalah game yang sedang tidak ada di queue \n");
-        printf("                 dan bukan merupakan 5 game pertama pada file konfigurasi.\n");
-        printf("QUEUE GAME     : Menambahkan permainan pilihan ke dalam daftar antrian game\n");
-        printf("PLAY GAME      : Memainkan game pertama dalam daftar antrian game\n");
-        printf("SKIPGAME <n>   : Menghapus n buah game dari daftar antrian dan memainkan game ke-(n+1)\n");
-        printf("QUIT           : Keluar dari game console\n");
-        printf("HELP           : Menampilkan daftar command\n\n");
-        printf("---------------------------------------------------------------------------------------\n");
+        printf("===================  PERINTAH  ===================");
+        printf("SAVE <filename> : Menyimpan keadaan game ke dalam file\n");
+        printf("CREATE GAME     : Menambahkan game baru pada daftar game\n");
+        printf("LIST GAME       : Menampilkan daftar game yang ada\n");
+        printf("DELETE GAME     : Menghapus sebuah game dari daftar game\n");
+        printf("                  Game yang dapat dihapus hanyalah game yang sedang tidak ada di queue \n");
+        printf("                  dan bukan merupakan 5 game pertama pada file konfigurasi.\n");
+        printf("HISTORY <n>     : Menampilkan n buah permainan yang terakhir dimainkan.");
+        printf("DELETE HISTORY  : Menghapus seluruh history.");
+        printf("SCOREBOARD      : Menampilkan daftar skor dan pemain tiap game.");
+        printf("RESET SCOREBOARD: Menghapus seluruh scoreboard.");        
+        printf("QUEUE GAME      : Menambahkan permainan pilihan ke dalam daftar antrian game\n");
+        printf("PLAY GAME       : Memainkan game pertama dalam daftar antrian game\n");
+        printf("SKIPGAME <n>    : Menghapus n buah game dari daftar antrian dan memainkan game ke-(n+1)\n");
+        printf("QUIT            : Keluar dari game console\n");
+        printf("HELP            : Menampilkan daftar command\n\n");
+        printf("---------------------------------------------------------------------------------------\n\n");
+
+        printf("=============  KETERANGAN PERMAINAN  =============");
+        printf("RNG            : Permainan menebak angka.");
+        printf("DINER DASH     : Permainan memasak dan menyajikan makanan.");
+        printf("HANGMAN        : Permainan menebak huruf-huruf dari sebuah kata.");
+        printf("TOWER OF HANOI : Permainan menggeser balok-balok dari tiang kiri ke tiang kanan.");
+        printf("                 Balok yang lebih besar tidak boleh ditaruh di atas balok yang lebih kecil.");
+        printf("SNAKE ON METEOR: Permainan menggerakkan ular untuk memakan makanan dan menghindari meteor dan obstacle.");
+        printf("---------------------------------------------------------------------------------------\n\n");
     }
     printf("Ketik command yang ingin Anda jalankan. Kemudian ketik enter untuk menjalankannya.\n");
     printf("Selamat bermain!~~\n");
